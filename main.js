0d501bb60df724785ff902ddf7c8776e69851ff1
Hafid <hafidkarkouch5@gmail.com>
2026-02-08 17:13:01 +0100

    restaure sommme reglage

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ const btn = document.getElementById("mobile-btn");
 const mobileNav = document.getElementById("mobile-nav");
 
 btn.addEventListener("click", () => {
-    mobileNav.classList.toggle("hidden");
+  mobileNav.classList.toggle("hidden");
 });
